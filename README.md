3710026d3c2126035d608fadf8cf8f1546c721b9
糖饼 <sugarpie.tang@gmail.com>
2021-12-05 13:33:38 +0800

    0.0.0

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # web-widget-manifest
+
 A file format for describing web widget applications.
 
 The schema is published as a [JSON Schema](https://json-schema.org/) file, in `schema.json`. The schema is written in TypeScript (see [schema.d.ts](https://github.com/web-widget/web-widget-manifest/blob/master/schema.d.ts)) and then compiled to JSON Schema.
@@ -40,3 +41,17 @@ The schema has a `schemaVersion` field in the top-level object to facilitate
 evolution of the schema. The schema follows [semver](https://semver.org/) versioning, the current schema version is `1.0.0`.
 
 This version will not always match the npm package version, as some changes to the npm package might not have changes to the schema. We will publish a list of schema versions and their associated npm versions and git tags.
+
+# Motivation
+
+Many tools need some machine-readable descriptions of web widget applications: IDEs, documentation viewers, linters, graphical design tools, etc.
+
+There have been several efforts in this area, including:
+
+- [custom-elements-manifest](https://github.com/webcomponents/custom-elements-manifest)
+- [Web Application Manifest](https://www.w3.org/TR/appmanifest/)
+- [Packaged Web Apps (Widgets)](https://www.w3.org/TR/2018/OBSL-widgets-20181011)
+- [Chrome Extensions: Manifest file format](https://developer.chrome.com/docs/extensions/mv3/manifest/)
+- [VS Code: Extension Manifest](https://code.visualstudio.com/api/references/extension-manifest)
+
+This repository is an effort to bring together tool owners to standardize on a common specification for a description format.
--- schema.d.ts
+++ schema.d.ts
@@ -8,10 +8,12 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
+import {JSONSchema7 as JSONSchema} from 'json-schema';
+
 /**
  * The top-level interface of a web widget application manifest file.
  */
- export interface Package {
+export interface Package {
   /**
    * The version of the schema used in this file.
    */
@@ -52,46 +54,12 @@ export interface WebWidgetApplication {
   description?: string;
 
   /**
-   * The declarations of a module.
-   *
-   * For documentation purposes, all declarations that are reachable from
-   * exports should be described here. Ie, functions and objects that may be
-   * properties of exported objects, or passed as arguments to functions.
+   * The declaration of a module.
    */
-  declarations?: Array<Declaration>;
-
+  declaration?: Declaration;
 }
 
 export type Declaration = WebWidgetApplicationDeclaration;
-
-/**
- * A reference to an export of a module.
- *
- * All references are required to be publically accessible, so the canonical
- * representation of a reference is the export it's available from.
- *
- * `package` should generally refer to an npm package name. If `package` is
- * undefined then the reference is local to this package. If `module` is
- * undefined the reference is local to the containing module.
- *
- * References to global symbols like `Array`, `HTMLElement`, or `Event` should
- * use a `package` name of `"global:"`.
- */
-export interface Reference {
-  name: string;
-  package?: string;
-  module?: string;
-}
-
-/**
- * A reference to the source of a declaration or member.
- */
-export interface SourceReference {
-  /**
-   * An absolute URL to the source (ie. a GitHub URL).
-   */
-  href: string;
-}
 
 /**
  * A description of a web widget application.
@@ -103,7 +71,7 @@ export type WebWidgetApplicationDeclaration = {
    */
   parameters?: Parameter[];
 
-  portals?: Portal[],
+  portals?: Portal[];
 
   /**
    * The shadow dom content slots that this element accepts.
@@ -114,14 +82,12 @@ export type WebWidgetApplicationDeclaration = {
 
   cssProperties?: CssCustomProperty[];
 
-  demos?: Demo[];
-
   data?: Data;
 
   sandboxed?: boolean;
 
-  // TODO sharedDependencies
-}
+  // TODO features, locales, sharedDependencies, placeholder, fallback, width, height
+};
 
 export interface Parameter {
   name: string;
@@ -137,17 +103,17 @@ export interface Parameter {
   description?: string;
 
   /**
-   * The type that the attribute will be serialized/deserialized as.
-   */
-  type?: Type;
-
-  /**
    * The default value of the attribute, if any.
    *
    * As parameters are always strings, this is the actual value, not a human
    * readable description.
    */
   default?: string;
+
+  /**
+   * Whether the parameter is optional. Undefined implies non-optional.
+   */
+  optional?: boolean;
 }
 
 export interface Portal {
@@ -232,85 +198,6 @@ export interface CssCustomProperty {
   description?: string;
 }
 
-export interface Type {
-  /**
-   * The full string representation of the type, in whatever type syntax is
-   * used, such as JSDoc, Closure, or TypeScript.
-   */
-  text: string;
-
-  /**
-   * An array of references to the types in the type string.
-   *
-   * These references have optional indices into the type string so that tools
-   * can understand the references in the type string independently of the type
-   * system and syntax. For example, a documentation viewer could display the
-   * type `Array<FooElement | BarElement>` with cross-references to `FooElement`
-   * and `BarElement` without understanding arrays, generics, or union types.
-   */
-  references?: TypeReference[];
-
-  source?: SourceReference;
-}
-
-/**
- * A reference that is associated with a type string and optionally a range
- * within the string.
- *
- * Start and end must both be present or not present. If they're present, they
- * are indices into the associated type string. If they are missing, the entire
- * type string is the symbol referenced and the name should match the type
- * string.
- */
-export interface TypeReference extends Reference {
-  start?: number;
-  end?: number;
-}
-
-/**
- * The common interface of variables, class fields, and function
- * parameters.
- */
-export interface PropertyLike {
-  name: string;
-
-  /**
-   * A markdown summary suitable for display in a listing.
-   */
-  summary?: string;
-
-  /**
-   * A markdown description of the field.
-   */
-  description?: string;
-
-  type?: Type;
-
-  default?: string;
-}
-
-export interface Parameter extends PropertyLike {
-  /**
-   * Whether the parameter is optional. Undefined implies non-optional.
-   */
-  optional?: boolean;
-}
-
-export interface Demo {
-  /**
-   * A markdown description of the demo.
-   */
-  description?: string;
-
-  /**
-   * Relative URL of the demo if it's published with the package. Absolute URL
-   * if it's hosted.
-   */
-  url: string;
-
-  source?: SourceReference;
-}
-
 export interface Data {
   name: string;
 
@@ -327,7 +214,7 @@ export interface Data {
   /**
    * The type that the data will be serialized/deserialized as.
    */
-  type?: Type;
+  declaration?: JSONSchema;
 
   /**
    * The default value of the data, if any.
